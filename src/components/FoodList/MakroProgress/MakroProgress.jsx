9f7133a6c66e3a242398411d82311829b3c3a6cf
atek <53567837+Dawidsjd@users.noreply.github.com>
2024-01-08 11:09:53 +0100

    adding responsiveness to circurallProgress

--- src/components/FoodList/MakroProgress/MakroProgress.jsx
+++ src/components/FoodList/MakroProgress/MakroProgress.jsx
@@ -7,12 +7,22 @@ import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import AddchartIcon from '@mui/icons-material/Addchart';
-import styles from './style';
+import { 
+  WidgetStatsContainer,
+  RightAlignContainer,
+  StatsContainer,
+  ProgressContainer,
+  StatsElement,
+  MakroContent,
+  InfoProgressContainer,
+  ProgressIcon
+ } from './style';
 import { CircularProgress } from '@mui/material';
 import Carbs from '../../../assets/Group 1.svg'
 import Fats from '../../../assets/Group 2.svg'
 import Protein from '../../../assets/Group 3.svg'
 import Calories from '../../../assets/Group 4.svg'
+import './media.css';
 
 const MakroProgress = ({ draggedRecipes }) => {
   const [totalCalories, setTotalCalories] = useState(0);
@@ -65,130 +75,135 @@ const MakroProgress = ({ draggedRecipes }) => {
   };
 
   return (
-    <div style={styles.WidgetStatsContainer}>
-      
+    <WidgetStatsContainer>
+      <StatsContainer>
+        {/* CircuralProgress */}
+        <ProgressContainer>
+          {['carbohydrates', 'fat', 'protein', 'calories'].map((macro, index) => (
+            <ProgressIcon key={index}>
+              <CircularProgress
+                variant="determinate"
+                value={Math.min(
+                  calculateGoalPercentage(
+                    macro === 'calories'
+                      ? totalCalories
+                      : macro === 'fat'
+                      ? totalFat
+                      : macro === 'protein'
+                      ? totalProtein
+                      : totalCarbohydrates,
+                    dailyGoals[macro]
+                  ),
+                  100 // Maksymalna wartość dla CircularProgress
+                )}
+                size={[65, 85, 110, 140][index]}
+                style={{ color: colors[index] }}
+              />
+            </ProgressIcon>
+          ))}
+        </ProgressContainer>
 
-      <div style={styles.StatsContainer}>
-      <div style={styles.ProgressContainer}>
-        {['carbohydrates', 'fat', 'protein', 'calories'].map((macro, index) => (
-          <div key={index} style={styles.ProgressIcon}>
-            <CircularProgress
-              variant="determinate"
-              value={Math.min(
-                calculateGoalPercentage(
-                  macro === 'calories'
-                    ? totalCalories
-                    : macro === 'fat'
-                    ? totalFat
-                    : macro === 'protein'
-                    ? totalProtein
-                    : totalCarbohydrates,
-                  dailyGoals[macro]
-                ),
-                100 // Maksymalna wartość dla CircularProgress
-              )}
-              size={[65, 85, 110, 140][index]}
-              style={{ color: colors[index] }}
-            />
-          </div>
-        ))}
-      </div>
-        <div style={styles.RightAlignContainer}>
+        {/* ikona stats */}
+        <RightAlignContainer>
           <Tooltip title="Open Macro Goals">
             <IconButton onClick={handleOpenDialog}>
               <AddchartIcon style={{ color: '111' }} />
             </IconButton>
           </Tooltip>
-        </div>
+        </RightAlignContainer>
 
-      <Dialog open={openDialog} onClose={handleCloseDialog} maxWidth="md">
-        <DialogTitle>Macro Goals</DialogTitle>
-        <DialogContent>
-          <p>Set your daily goals for macros:</p>
-          <div>
-            <label>Calories:</label>
-            <input
-              type="number"
-              value={dailyGoals.calories}
-              onChange={(e) => handleDailyGoalChange(e, 'calories')}
-            />
-          </div>
-          <div>
-            <label>Fat:</label>
-            <input
-              type="number"
-              value={dailyGoals.fat}
-              onChange={(e) => handleDailyGoalChange(e, 'fat')}
-            />
-          </div>
-          <div>
-            <label>Protein:</label>
-            <input
-              type="number"
-              value={dailyGoals.protein}
-              onChange={(e) => handleDailyGoalChange(e, 'protein')}
-            />
-          </div>
-          <div>
-            <label>Carbohydrates:</label>
-            <input
-              type="number"
-              value={dailyGoals.carbohydrates}
-              onChange={(e) => handleDailyGoalChange(e, 'carbohydrates')}
-            />
-          </div>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseDialog}>Close</Button>
-        </DialogActions>
-      </Dialog>
-      
-        <div style={styles.InfoProgressContainer}>
+        {/* Okno PopUp */}
+        <Dialog open={openDialog} onClose={handleCloseDialog} maxWidth="md">
+          <DialogTitle>Macro Goals</DialogTitle>
+          <DialogContent>
+            <p>Set your daily goals for macros:</p>
+            <div>
+              <label>Calories:</label>
+              <input
+                type="number"
+                value={dailyGoals.calories}
+                onChange={(e) => handleDailyGoalChange(e, 'calories')}
+              />
+            </div>
+            <div>
+              <label>Fat:</label>
+              <input
+                type="number"
+                value={dailyGoals.fat}
+                onChange={(e) => handleDailyGoalChange(e, 'fat')}
+              />
+            </div>
+            <div>
+              <label>Protein:</label>
+              <input
+                type="number"
+                value={dailyGoals.protein}
+                onChange={(e) => handleDailyGoalChange(e, 'protein')}
+              />
+            </div>
+            <div>
+              <label>Carbohydrates:</label>
+              <input
+                type="number"
+                value={dailyGoals.carbohydrates}
+                onChange={(e) => handleDailyGoalChange(e, 'carbohydrates')}
+              />
+            </div>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseDialog}>Close</Button>
+          </DialogActions>
+        </Dialog>
 
-          <div style={styles.StatsElement}>
+        <InfoProgressContainer>
+          {/* Element Makro */}
+          <StatsElement>
             <img src={Carbs} alt="Carbs" />
-            <div style={styles.MakroContent}>
-              <h2 style={{color: '#6A8D73'}}>Carbs</h2>
+            <MakroContent>
+              <h2 style={{ color: '#6A8D73' }}>Carbs</h2>
               <p>
-              {calculateGoalPercentage(totalCarbohydrates, dailyGoals.carbohydrates)}% {totalCarbohydrates}g
+                {calculateGoalPercentage(totalCarbohydrates, dailyGoals.carbohydrates)}% {totalCarbohydrates}g
               </p>
-            </div>
-          </div>
+            </MakroContent>
+          </StatsElement>
 
-          <div style={styles.StatsElement}>
+          {/* Element Makro */}
+          <StatsElement>
             <img src={Fats} alt="Fats" />
-            <div style={styles.MakroContent}>
-            <h2 style={{color: '#9F8BE8'}}>Fats</h2>
+            <MakroContent>
+              <h2 style={{ color: '#9F8BE8' }}>Fats</h2>
               <p>
-              {calculateGoalPercentage(totalFat, dailyGoals.fat)}% {totalFat}g
+                {calculateGoalPercentage(totalFat, dailyGoals.fat)}% {totalFat}g
               </p>
-            </div>
-          </div>
-        </div>
-        <div style={styles.InfoProgressContainer}>
-        <div style={styles.StatsElement}>
+            </MakroContent>
+          </StatsElement>
+        </InfoProgressContainer>
+
+        <InfoProgressContainer>
+          {/* Element Makro */}
+          <StatsElement>
             <img src={Protein} alt="Protein" />
-            <div style={styles.MakroContent}>
-            <h2 style={{color: '#F0A868'}}>Protein</h2>
+            <MakroContent>
+              <h2 style={{ color: '#F0A868' }}>Protein</h2>
               <p>
-              {calculateGoalPercentage(totalProtein, dailyGoals.protein)}% {totalProtein}g
+                {calculateGoalPercentage(totalProtein, dailyGoals.protein)}% {totalProtein}g
               </p>
-            </div>
-          </div>
-          <div style={styles.StatsElement}>
+            </MakroContent>
+          </StatsElement>
+
+          {/* Element Makro */}
+          <StatsElement>
             <img src={Calories} alt="Calories" />
-            <div style={styles.MakroContent}>
-            <h2 style={{color: '#000000'}}>Calories</h2>
+            <MakroContent>
+              <h2 style={{ color: '#000000' }}>Calories</h2>
               <p>
                 {calculateGoalPercentage(totalCalories, dailyGoals.calories)}% {totalCalories}g
               </p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      
-    </div>
+            </MakroContent>
+          </StatsElement>
+        </InfoProgressContainer>
+      </StatsContainer>
+    </WidgetStatsContainer>
   );
 };
 
--- src/components/FoodList/MakroProgress/style.js
+++ src/components/FoodList/MakroProgress/style.js
@@ -1,53 +1,87 @@
-const styles = {
+import { styled } from "@mui/material/styles";
+import globalTheme from '../../../globalTheme/globalTheme';
 
-    RightAlignContainer: {
-        position: 'absolute',
-        top: '1em',
-        right: '1em',
-      },
-      WidgetStatsContainer: {
-        position: 'relative',
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        
-      },
-    StatsContainer: {
-        position: 'relative',
-        width: '90%',
-        height: '80%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        background: "rgba(34, 34, 34, 0.14)",
-    backdropFilter: "blur(9.3px)",
-    boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
-    borderRadius: '10px'
-    },
-    ProgressContainer: {
-        position: 'relative',
-        marginRight: '5em',
-    },
-    StatsElement: {
-        display: 'flex',
-        margin: '2em 0 2em 2em'
-    },
-    MakroContent: {
-        marginLeft: '1.5em'
-    },
-    InfoProgressContainer: {
-        margin: '1em'
-    },
-    ProgressIcon: {
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)', // Przesunięcie do środka
-          },
+export const RightAlignContainer = styled('div')(({ theme }) => ({
+  position: 'absolute',
+  top: '1em',
+  right: '1em',
+}));
 
-  };
+export const WidgetStatsContainer = styled('div')(({ theme }) => ({
+  position: 'relative',
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+}));
+
+export const StatsContainer = styled('div')(({ theme }) => ({
+  position: 'relative',
+  width: '90%',
+  height: '80%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  background: "rgba(34, 34, 34, 0.14)",
+  backdropFilter: "blur(9.3px)",
+  boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+  borderRadius: '10px',
+  [theme.breakpoints.down("1459")]: {
+    flexDirection: 'column'
+  },
+}));
+
+export const ProgressContainer = styled('div')(({ theme }) => ({
+  position: 'relative',
+  marginRight: '5em',
+  [theme.breakpoints.down("1639")]: {
+    marginRight: '2.5em',
+  },
+  [theme.breakpoints.down("1565")]: {
+    marginRight: '1em',
+    transform: 'scale(0.7)'
+  },
+  [theme.breakpoints.down("1459")]: {
+    transform: 'scale(0.6)',
+    
+    margin: '1em 0 0 1em'
+  },
   
-  export default styles;
+}));
+
+export const StatsElement = styled('div')(({ theme }) => ({
+  display: 'flex',
+  margin: '2em 0 2em 2em',
   
+}));
+
+export const MakroContent = styled('div')(({ theme }) => ({
+  marginLeft: '1.5em',
+  [theme.breakpoints.down("1459")]: {
+    marginLeft: '0',
+    marginRight: '1em'
+  },
+}));
+
+export const InfoProgressContainer = styled('div')(({ theme }) => ({
+  margin: '1em',
+  [theme.breakpoints.down("1459")]: {
+    display: 'flex',
+    margin: '0',
+    height: '30%',
+    width: '100%',
+    justifyContent: 'space-evenly',
+    alignItems: 'stretch',
+    "& img": {
+        display: 'none'
+      },
+  },
+}));
+
+export const ProgressIcon = styled('div')(({ theme }) => ({
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+}));
