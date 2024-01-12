7c1746c0544743ca758b0397a662027ec3047e30
Dawidsjd <dawid.buszta2005@gmail.com>
2024-01-12 20:16:40 +0100

    menu -setup

--- src/components/Cookbook/FoodCarousel/FoodCarousel.jsx
+++ src/components/Cookbook/FoodCarousel/FoodCarousel.jsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Carousel from 'better-react-carousel';
+import Icon from '../../../assets/unfavourite.svg'
+
+import style, { CarouselCon, Divv, IconContainer } from './styles';
+import './keyframe.css';
 
 const FoodCarousel = () => {
   const [recipes, setRecipes] = useState([]);
@@ -7,13 +11,14 @@ const FoodCarousel = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes';
+        const url =
+          'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes';
         const options = {
           method: 'GET',
           headers: {
             'X-RapidAPI-Key': 'b459219d15mshc4c14541f610567p1c8df9jsn45d71054de0a',
-            'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-          }
+            'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
+          },
         };
 
         const response = await fetch(url, options);
@@ -29,16 +34,30 @@ const FoodCarousel = () => {
   }, []);
 
   return (
-    <Carousel cols={2} rows={1} gap={10} loop showDots responsiveLayout={[{ breakpoint: 800, cols: 3 }]}>
+    <Carousel
+      cols={4}
+      rows={1}
+      gap={10}
+      loop
+      showDots
+      responsiveLayout={[{ breakpoint: 800, cols: 3 }]}
+    >
       {recipes.map((recipe) => (
         <Carousel.Item key={recipe.id}>
-          <img
-            width="100%"
-            height="200px"  // Ustaw stałą wysokość dla wszystkich zdjęć
-            src={recipe.thumbnail_url}
-            alt={recipe.name}
-          />
-          <p>{recipe.name}</p>
+          <Divv>
+            <img
+              style={{ width: '95%', height: '100px', objectFit: 'cover' }}
+              src={recipe.thumbnail_url}
+              alt={recipe.name}
+            />
+            <p className={recipe.thumbnail_url && recipe.thumbnail_url.length > 200 ? 'animated-text' : ''}>
+              {recipe.name}
+            </p>
+            
+            <IconContainer>
+              <img src={Icon} alt="" />
+            </IconContainer>
+          </Divv>
         </Carousel.Item>
       ))}
     </Carousel>
--- src/components/Cookbook/FoodCarousel/styles.js
+++ src/components/Cookbook/FoodCarousel/styles.js
@@ -1,18 +1,32 @@
-// import { styled } from "@mui/system";
+import { styled } from "@mui/system";
 
-// export const MenuCookContainer = styled('div')(({ theme }) => ({
-//     position: "absolute",
-//     top: 0,
-//     left: 0,
-//     width: '-webkit-fill-available',
-//     height: "100%",
-// }));
+export const Divv = styled('div')(({ theme }) => ({
+  position: 'relative',
+  width: '200px',
+  height: '130px',
+  background: '#86BF80',
+  margin: 'auto',  // Wysrodkowanie horyzontalne
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',  // Wysrodkowanie pionowe
+  justifyContent: 'center',  // Wysrodkowanie pionowe
+  borderRadius: '10px',
+  overflow: 'hidden',
+  "& img": {
+    borderRadius: '10px',
+  },
+  "& p": {
+    whiteSpace: 'nowrap',  // Zapobiega przewijaniu tekstu do nowej linii
+    overflow: 'hidden',   // Ukrywa nadmiarowy tekst
+    textOverflow: 'ellipsis',  // Dodaje trzy kropki na końcu tekstu
+    margin: '0',   // Usuwa marginesy dla elementu p
+  },
+}));
 
-// export const CookBookContainer = styled('div')({
-//     position: "absolute",
-//     top: 0,
-//     right: 0,
-//     width: "80%",
-//     height: "100%",
-   
-// });
+export const IconContainer = styled('div')(({ theme }) => ({
+    position: "absolute",
+  top: "0",
+  right: "0",
+  padding: "5px",
+  transform: 'scale(0.8)'
+  }));
