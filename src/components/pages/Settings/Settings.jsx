870ba0937b96400fe03eb4dd221734023e03b530
bezoski <rafalbuczkowski2004@wp.pl>
2024-01-18 09:11:26 +0100

    settings setup

--- src/components/pages/Settings/Settings.jsx
+++ src/components/pages/Settings/Settings.jsx
@@ -1,13 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import Menu from "../../Menu/Menu";
-import { SettingsContainer, SettingsPanel, SettingsBox } from "./styles";
+import {
+  SettingsContainer,
+  SettingsPanel,
+  SettingsBox,
+  SectionHeader,
+  SectionLinks,
+  LinkBtn,
+  ActiveSection,
+} from "./styles";
+import General from "./General/General";
+import Security from "./Security/Security";
+import Appearance from "./Appearance/Appearance";
 
 const Settings = () => {
+  const [activeLink, setActiveLink] = useState("general");
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
+
   return (
     <SettingsContainer>
       <Menu />
       <SettingsPanel>
-        <SettingsBox>settings</SettingsBox>
+        <SettingsBox>
+          <SectionHeader>
+            <p className="header">Settings</p>
+            <SectionLinks>
+              <LinkBtn
+                className={activeLink === "general" ? "active" : ""}
+                onClick={() => handleLinkClick("general")}
+              >
+                General
+              </LinkBtn>
+              <LinkBtn
+                className={activeLink === "appearance" ? "active" : ""}
+                onClick={() => handleLinkClick("appearance")}
+              >
+                Appearance
+              </LinkBtn>
+              <LinkBtn
+                className={activeLink === "security" ? "active" : ""}
+                onClick={() => handleLinkClick("security")}
+              >
+                Security
+              </LinkBtn>
+            </SectionLinks>
+          </SectionHeader>
+          <ActiveSection>
+            {activeLink === "general" && <General />}
+            {activeLink === "appearance" && <Appearance />}
+            {activeLink === "security" && <Security />}
+          </ActiveSection>
+        </SettingsBox>
       </SettingsPanel>
     </SettingsContainer>
   );
--- src/components/pages/Settings/styles.js
+++ src/components/pages/Settings/styles.js
@@ -10,7 +10,6 @@ export const SettingsContainer = styled("div")(({ theme }) => ({
 export const SettingsPanel = styled("div")(({ theme }) => ({
   border: "1px solid black",
   flex: 1,
-  padding: "20px",
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
@@ -18,10 +17,25 @@ export const SettingsPanel = styled("div")(({ theme }) => ({
 
 export const SettingsBox = styled("div")(({ theme }) => ({
   background: globalTheme.palette.background.additional,
+  borderRadius: "15px",
+  width: "80%",
+  boxShadow: "2px 1px 5px -2px rgba(0, 0, 0, 1)",
 }));
 
-// export const SettingsPanel = styled("div")(({ theme }) => ({}));
+export const SectionHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  boxShadow: "0px 4px 8px -2px rgba(0, 0, 0, 0.1)",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  width: "100%",
+  padding: "15px",
+}));
 
-// export const SettingsPanel = styled("div")(({ theme }) => ({}));
+export const SectionLinks = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "flex-end",
+}));
 
-// export const SettingsPanel = styled("div")(({ theme }) => ({}));
+export const LinkBtn = styled("button")(({ theme }) => ({}));
+
+export const ActiveSection = styled("div")(({ theme }) => ({}));
