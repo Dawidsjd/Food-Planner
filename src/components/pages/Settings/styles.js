e3ee0fdff68aaf560ee6c5cbadd08dfa8b2d61b3
bezoski <rafalbuczkowski2004@wp.pl>
2024-01-18 09:36:05 +0100

    settings header styled

--- src/components/pages/Settings/styles.js
+++ src/components/pages/Settings/styles.js
@@ -20,6 +20,11 @@ export const SettingsBox = styled("div")(({ theme }) => ({
   borderRadius: "15px",
   width: "80%",
   boxShadow: "2px 1px 5px -2px rgba(0, 0, 0, 1)",
+
+  // Responsywność - telefon
+  [theme.breakpoints.down("md")]: {
+    width: "90%",
+  },
 }));
 
 export const SectionHeader = styled("div")(({ theme }) => ({
@@ -28,7 +33,22 @@ export const SectionHeader = styled("div")(({ theme }) => ({
   flexDirection: "row",
   justifyContent: "space-between",
   width: "100%",
-  padding: "15px",
+  padding: "30px",
+
+  "& .header": {
+    fontSize: globalTheme.typography.subtitle.main,
+    fontWeight: 600,
+
+    // Responsywność - telefon
+    [theme.breakpoints.down("md")]: {
+      fontSize: globalTheme.typography.primary.main,
+    },
+  },
+
+  // Responsywność - telefon
+  [theme.breakpoints.down("md")]: {
+    padding: "15px",
+  },
 }));
 
 export const SectionLinks = styled("div")(({ theme }) => ({
@@ -36,6 +56,39 @@ export const SectionLinks = styled("div")(({ theme }) => ({
   alignItems: "flex-end",
 }));
 
-export const LinkBtn = styled("button")(({ theme }) => ({}));
+export const LinkBtn = styled("button")(({ theme }) => ({
+  margin: "0 10px",
+  border: "none",
+  background: "none",
+  cursor: "pointer",
+  position: "relative",
+  textDecoration: "none",
+  fontSize: globalTheme.typography.primary.main,
+
+  // Responsywność - telefon
+  [theme.breakpoints.down("md")]: {
+    fontSize: globalTheme.typography.secondary.main,
+    margin: "0 5px",
+  },
+
+  "&:after": {
+    content: '""',
+    position: "absolute",
+    background: globalTheme.palette.primary.main,
+    height: "3px",
+    width: 0,
+    left: 0,
+    bottom: "-5px",
+    transition: "0.3s",
+  },
+
+  "&:hover:after": {
+    width: "100%",
+  },
+
+  "&.active:after": {
+    width: "100%",
+  },
+}));
 
 export const ActiveSection = styled("div")(({ theme }) => ({}));
